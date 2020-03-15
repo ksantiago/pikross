73f31cc5cb192bcbe5b1f7a40d56919d67d410a8
Katherine Santiago <santiago.kat@gmail.com>
2020-03-15 16:22:52 -0400

    feat: added browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Home from './client/home'
 import Board from './client/board'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Board />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/board" component={Board} />
+      </div>
+    </Router>
   );
 }
 
--- src/client/home.js
+++ src/client/home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-// import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
         <h1>Welcome to Picross</h1>
-        {/* <Link to="/board"><button>1 Player</button></Link> */}
-        {/* <Link to="/board"><button disabled>2 Player</button></Link> */}
+        <Link to="/board"><button>1 Player</button></Link>
+        <Link to="/board"><button disabled>2 Player</button></Link>
     </div>
   )
 }
